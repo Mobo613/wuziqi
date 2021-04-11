b76353f8fb2ab3b0828e5df9ab391756620d1d97
Chen Jiaqi <chenjiaqi613@gmail.com>
2021-04-11 22:15:48 +0800

    Revert "Revert "feat: 添加棋盘的点击事件""
    This reverts commit 200a99b7046f833b7037aae40c1314b81af1272c.

---
+++ Assets/Scripts/CellClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wuziqi.Cell
+{
+	/// <summary>
+	/// 棋盘点击
+	/// </summary>
+	public class CellClick : MonoBehaviour
+	{
+        private void OnMouseDown()
+        {
+            print("Position: ( " + this.transform.position.x + " , " + this.transform.position.y + " )");
+        }
+    }
+}
